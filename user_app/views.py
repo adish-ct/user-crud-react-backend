b882b723ea7de557147c93ef4ab58edaa833e6d6
adish-ct <adishathu525@gmail.com>
2023-10-25 18:46:33 +0530

    register logic partially written

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+
+        first_name = data["first_name"]
+        last_name = data["last_name"]
+        email = data["email"]
+        password = data["password"]
+
+        return Response({}, status=status.HTTP_201_CREATED)
+
+
+class RetriveUserView(APIView):
+    # only authenticted user call this api
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        pass
