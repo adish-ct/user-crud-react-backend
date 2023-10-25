170be2aad60934b3946bf8c26d177a90302afe7f
adish-ct <adishathu525@gmail.com>
2023-10-25 19:38:43 +0530

    added logic file

---
+++ LogicFile.py
@@ -0,0 +1,10 @@
+#  Added logic in this file
+
+# serializer = UserCreateSerializer(
+#     data={
+#         "first_data": data["first_name"],
+#         "last_name": data["last_name"],
+#         "email": data["email"],
+#         "password": data["password"],
+#     }
+# )
